22c79384d29ec595036403015d0402facf19f96e
mdsujan-mriddha <mdsujan134523@gmail.com>
2024-07-28 16:27:59 +0600

    init project

---
+++ utils/log.js
@@ -0,0 +1,14 @@
+export const displayFancyLogs = () => {
+  console.log(
+    "%c             _             \n  ___ _   _ (_) __ _ _ __  \n / __| | | || |/ _` | '_ \\ \n \\__ \\ |_| || | (_| | | | |\n |___/\\__,_|/ |\\__,_|_| |_|\n          |__/             ",
+    "background: #212121; color: #6b17e8;"
+  );
+  
+  
+  
+
+  console.log(
+    "%c Hope you like what you see :)",
+    "background: #212121; color: #6b17e8; padding: 6px;"
+  );
+};
